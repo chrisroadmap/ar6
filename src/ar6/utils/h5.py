eb27bd3717c4d20d4ebac731b6d81a694ea4cb4e
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2020-11-20 11:42:51 +1100

    Add regression test (ish) before refactoring

--- src/ar6/utils/h5.py
+++ src/ar6/utils/h5.py
@@ -40,7 +40,10 @@ def recursively_load_dict_contents_from_group(h5file, path):
     ans = {}
     for key, item in h5file[path].items():
         if isinstance(item, h5py._hl.dataset.Dataset):
-            ans[key] = item.value
+            try:
+                ans[key] = item.value
+            except AttributeError:
+                ans[key] = item[:]
         elif isinstance(item, h5py._hl.group.Group):
             ans[key] = recursively_load_dict_contents_from_group(h5file, path + key + '/')
     return ans
