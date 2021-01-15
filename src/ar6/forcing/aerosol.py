b023b1001aa921f9d8dca16e4395e0c65de97588
Chris Smith <chrisroadmap@gmail.com>
2021-01-15 10:08:34 +0000

    add ghan function for indirect aerosol and track ozone to Thornhill et al 2020

--- src/ar6/forcing/aerosol.py
+++ src/ar6/forcing/aerosol.py
@@ -30,6 +30,32 @@ def aerocom_n(x, bc, oc, so2, nh3):
     return bc*x[0] + oc*x[1] + so2*x[2] + nh3*x[3]
 
 
+def ghan(x, beta, n0, n1):
+    """ERFaci logarithmic in emissions excluding nitrate.
+
+    Named after Steve Ghan, whose 2013 simple emissions emulator is extremely useful,
+    (https://agupubs.onlinelibrary.wiley.com/doi/full/10.1002/jgrd.50567),
+    and can be emulated again using this very simple formula.
+
+    Inputs
+    ------
+    x : obj:`numpy.array`
+        Time series of aerosol emissions
+    beta : float
+        Scale factor linking forcing to time series
+    n0 : float
+        Shape factor for SO2 emissions, W m**-2 (TgSO2 yr**-1)**-1
+    n1 : float
+        Shape factor for BC+OC emissions, W m**-2 (TgC yr**-1)**-1
+
+    Returns
+    -------
+    res : obj:`numpy.array`
+        Time series of ERFaci
+    """
+    return -beta*np.log(1 + x[0]/n0 + x[1]/n1)
+
+
 def shindell(x, beta, n0, n1, n2):
     """ERFaci logarithmic in emissions including nitrate.
 
